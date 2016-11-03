17033861ba3faef28be249ebabf4ea6d30fea377
Lucas Machado <lucas.machado290@gmail.com>
2016-11-03 10:59:06 -0200

    Writing client response - Add faker dependency - Add , http, url and querystring module - Add server and listener on 3000 port

--- fakemytemplate/index.js
+++ fakemytemplate/index.js
@@ -1,4 +1,20 @@
-const faker  = require('faker');
+const faker = require('faker');
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
 
-const randomName = faker.name.findName();
-console.log(randomName);
+const server = http.createServer();
+server.on('request', function(request, response) {
+	const parsedUrl = url.parse(request.url);
+	const parameters = querystring.parse(parsedUrl.query);
+	
+	console.log('parameters: ', parameters);
+	
+	const result = faker.fake(parameters.template);
+	
+	response.write(result);
+	response.end();
+});
+
+server.listen(3000);
+console.log('Server is running.');
